74e42788badc771a10df2d64356cb79fec2303cb
MoEzwawi <mezwawi96@gmail.com>
2024-01-08 13:40:53 +0100

    initializes spring project

---
+++ src/main/java/MoEzwawi/BES5L1/BeS5L1Application.java
@@ -0,0 +1,13 @@
+package MoEzwawi.BES5L1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeS5L1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeS5L1Application.class, args);
+	}
+
+}
---
+++ src/test/java/MoEzwawi/BES5L1/BeS5L1ApplicationTests.java
@@ -0,0 +1,13 @@
+package MoEzwawi.BES5L1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeS5L1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
